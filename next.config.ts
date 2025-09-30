c1fce4d0703b09575e3944b398fe571b67190c51
Sebastian Brizuela <sebita29@gmail.com>
2025-09-30 20:35:41 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',          // ← Esta línea es la clave
   typescript: {
     ignoreBuildErrors: true,
   },
